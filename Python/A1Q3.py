ff15492e4baf59bea59b5ac6d7c550dceb0cf3ac
Ramin Roufeh <raminroufeh@gmail.com>
2021-07-27 10:12:00 -0400

    Create A1Q3.py

---
+++ Python/A1Q3.py
@@ -0,0 +1,9 @@
+#Create a python script that will calculate/display:
+# Names, types and sizes of blobs in a certain container
+# Names and sizes of “folders” in a certain container
+# connection_string =
+# "DefaultEndpointsProtocol=https;AccountName=antrablobstorage;AccountKey=E
+# CVP9sDWl64Ubd6w3lGd4d4fbiZuwHWWu1q/KoS2sCR18mwwkSxf1gLC7PvqC
+# T1jWi3IYE87ZQtJYMIztIg3vg==;EndpointSuffix=core.windows.net"
+# container_name = "imagescontainer"
+
