fb8506f43bbcba34813926863755a791973a8ba8
Ramin Roufeh <raminroufeh@gmail.com>
2021-07-27 10:48:35 -0400

    Update A1Q3.py

--- Python/A1Q3.py
+++ Python/A1Q3.py
@@ -7,3 +7,12 @@
 # T1jWi3IYE87ZQtJYMIztIg3vg==;EndpointSuffix=core.windows.net"
 # container_name = "imagescontainer"
 
+connection_string = '''
+DefaultEndpointsProtocol=https;AccountName=antrablobstorage;AccountKey=E
+CVP9sDWl64Ubd6w3lGd4d4fbiZuwHWWu1q/KoS2sCR18mwwkSxf1gLC7PvqC
+T1jWi3IYE87ZQtJYMIztIg3vg==;EndpointSuffix=core.windows.net'''
+
+container_name = 'imagescontainer'
+
+container = ContainerClient.from_connection_string(conn_str = connection_string, container_name = container_name)
+
