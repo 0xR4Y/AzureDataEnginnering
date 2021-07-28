38ec47b9c689f908bf20ea53f305dd114a0c11f8
Ramin Roufeh <raminroufeh@gmail.com>
2021-07-28 13:16:45 -0400

    Update A1Q2.py

--- Python/A1Q2.py
+++ Python/A1Q2.py
@@ -2,8 +2,10 @@
 
 import json
 
-def split_by(file: str, slices: int):
-    with open(file, encoding='utf8') as f:
+filename = 'data/movie.json'
+slices = 8
+
+with open(filename, encoding='utf8') as f:
         j = json.load(f)
         jarr = list(j['movie'])
         l = len(jarr)
@@ -11,10 +13,5 @@ def split_by(file: str, slices: int):
         for i in range(0, l, (l//slices)+1):
             end = i + (l//slices)+1
             count += 1
-            with open('movies_slices/'+str(count)+'.json', 'w+', encoding='utf8') as d:
+            with open('data/'+str(count)+'.json', 'w+', encoding='utf8') as d:
                 json.dump(jarr[i:end], d, indent=4)
-
-
-if __name__ == '__main__':
-    filename = 'movie.json'
-    split_by(filename, 8)
